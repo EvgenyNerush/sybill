bc9012418025a12d80d27867265f8cafd9da4836
Evgeny Nerush <eugene.nerush@gmail.com>
2023-01-02 00:02:35 +0300

    add btc data; linear and periodic approximations

--- src/get_data.clj
+++ src/get_data.clj
@@ -1,7 +1,9 @@
 (ns get-data
   (:require [clj-http.client :as client])
   (:require [cheshire.core :as json])
-  (:require [java-time.api :as jt]))
+  (:require [java-time.api :as jt])
+  (:require [clojure.java.io :as io])
+  (:require [clojure.string :as string]))
 
 (defn gecko-fmt
   "String representation of the date for Coingecko API"
@@ -13,13 +15,16 @@
 
 (defn price-at-date
   "Usd price of the given coin at the date. Wrapper of the Coigecko web API,
-   see https://www.coingecko.com/en/api/documentation"
+   see https://www.coingecko.com/en/api/documentation
+   Yields `nil` if any exception is raised by the get request"
   [coin-id date]
   (let [request (str "https://api.coingecko.com/api/v3/coins/"
                      coin-id
                      "/history?date="
                      (gecko-fmt date))
-        response (client/get request)
+        response (try
+                   (client/get request {:socket-timeout 10000 :connection-timeout 10000})
+                   (catch Exception e (do (println e) nil)))
         body-str (:body response)]
     (:usd (:current_price (:market_data (json/parse-string body-str true))))))
 
@@ -38,6 +43,25 @@
     (let [price (price-at-date coin-id date)]
       (if (and price (not (jt/after? date date-to)))
         (do (println (gecko-fmt date) price)
-            (Thread/sleep 2000)
+            (Thread/sleep 4000)
             (recur (jt/plus date (jt/days 1)) (conj res price)))
         res))))
+
+(defn write-prices
+  "Writes prices obtained from `get-prices` to a file"
+  [coin-id date-from date-to]
+  (let [prices (get-prices coin-id date-from date-to)
+        str-prices (reduce (fn [x y] (str x \newline y)) prices)
+        filename (str coin-id "-"
+                      (gecko-fmt date-from) "-"
+                      (gecko-fmt date-to)   ".txt")]
+    (spit filename str-prices)))
+
+(defn read-prices
+  "Reads prices from recources/filename"
+  [filename]
+  (mapcat (fn [s] (try [(Double/parseDouble s)]
+                       (catch Exception _ [])))
+          (string/split-lines (slurp (io/resource (str "resources/" filename))))))
+
+(assert (= (first (read-prices "bitcoin-01-10-2022-01-12-2022.txt")) 19476.92659600407) "read-prices")
--- src/sybill.clj
+++ src/sybill.clj
@@ -4,8 +4,9 @@
 
 ^{:nextjournal.clerk/visibility {:code :hide}}
 (ns sybill
-  (:require [nextjournal.clerk.viewer :as v])
-  (:require [clojure.math :as math]))
+  (:require [nextjournal.clerk.viewer :as v]
+            [nextjournal.clerk :as clerk]
+            [get-data :as data]))
 
 ;; First, we need some data - something that changes day by day.
 ;; The Bitcoin price serves well. It looks quite random and
@@ -16,6 +17,113 @@
 ;; crypto trading? If not, I warn now!)
 
 ;; *Coingecko* has nice [API](https://www.coingecko.com/en/api/documentation)
-;; to fetch historical data, so what about three previous years?
+;; to fetch historical data, so I downloaded some (with `write-prices` from
+;; `src/get_data.clj`). Below
+;; is the Bitcoin price in USD for a couple of months - from 1 Oct 2022
+;; to 1 Dec 2022:
 
+(def data-1 (data/read-prices "bitcoin-01-10-2022-01-12-2022.txt"))
 
+(v/plotly {:layout {:title "BTC in USD for two months"}
+           :data [{:x (range (count data-1))
+                   :y data-1
+                   :type "line"}]})
+
+;; The network we built
+;; is as simple as possible: it gets the prices during a week and predicts
+;; prices for the next week, hence it consist only of two layers (input
+;; and output) seven nodes each. Values of output nodes are computed as
+;; linear combinations of the input nodes:
+
+;; $$
+;; u_j = \sum_{i=1}^7 w_{ji} v_i,
+;; $$
+
+;; with $$ w_{ij} $$ weights, $$ v_i $$ values of the input layer (some week)
+;; and $$ u_j $$ values of the output layer (next week, prediction).
+;; No hidden layer is present here.
+
+;; ![image of two-layer neural network](resources/neural-network.png)
+
+;; This neural network is so simple that we can find quite good
+;; weights $$ w_{ij} $$ without any training. For instance, if the
+;; linear trend works well for most of weeks, it's good to use
+
+;; $$
+;; u_1 = v_7 + \frac{1}{6} (v_7 - v_1), \\
+;; u_2 = v_7 + \frac{2}{6} (v_7 - v_1), ...
+;; $$
+
+(def x14 (vec (range 1 15)))
+
+(def v-linear (map (fn [x] (+ x (rand))) x14))
+
+(def u-linear
+  (let [v7 (get (vec v-linear) 6)
+        v1 (get (vec v-linear) 0)
+        slope (/ (- v7 v1) 6)]
+    (map (fn [x] (+ v7 (* x slope))) (take 7 x14))))
+
+(v/plotly {:layout {:title "linear approximation"}
+           :data [{:x x14
+                   :y v-linear
+                   :type "line"
+                   :name "data"}
+                  {:x (drop 7 x14)
+                   :y u-linear
+                   :type "line"
+                   :name "approximation"}]})
+
+;; Here approximation is done with first seven points of the data,
+;; the last seven points of the data are present to show how good/bad
+;; the approximation is.
+
+;; The slope of the price curve can vary and it can be optimal to compute
+;; it not from a week interval, but from a shorter period. Or longer, if
+;; the data is quite noisy, for instance. One more variant of linear approximation
+;; is based on the slope computed not from difference of two points, but
+;; from difference of two groups of points, say $$ (v_7 + v_6 - v_1 - v_2) / 10 $$.
+
+;; One else good example is the approximation of periodic data. For example,
+;; if the price has period of five days, one can set
+
+;; $$
+;; u_1 = v_3, \quad u_2 = v_4, \quad u_3 = v_5, \quad u_4 = v_6, \\
+;; u_5 = v_7, \quad u_6 = v_3, \quad u_7 = v_4.
+;; $$
+
+;; Look how it works for some periodic data with noise:
+
+(def k (* 2 Math/PI 1/5))
+
+(def v-periodic-1 (map (fn [x] (Math/sin (* k (+ x (rand))))) x14))
+
+(def v-periodic-2 (map (fn [x] (Math/sin (* k (+ x 1.5 (rand))))) x14))
+
+(defn u-periodic
+  [vs]
+  (let [ys (drop 2 vs)]
+    (take 7 (concat ys ys))))
+
+(v/plotly {:layout {:title "approximation of periodic functions"}
+           :data [{:x x14
+                   :y v-periodic-1
+                   :type "line"
+                   :name "data 1"}
+                  {:x (drop 7 x14)
+                   :y (u-periodic v-periodic-1)
+                   :type "line"
+                   :name "approximation 1"}]})
+
+(v/plotly {:layout {:title "approximation of periodic functions"}
+           :data [{:x x14
+                   :y v-periodic-2
+                   :type "line"
+                   :name "data 2"}
+                  {:x (drop 7 x14)
+                   :y (u-periodic v-periodic-2)
+                   :type "line"
+                   :name "approximation 2"}]})
+
+;; 🤔 Imagine, how the matrix (tensor) $$ w_{ij} $$ looks like for linear approximation,
+;; for periodic approximation.
